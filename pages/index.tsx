26be6b51f25d64c70892d22a5a530775b3c20ae4
Mika Bona <dmitriygolubapk@gmail.com>
2022-05-27 12:56:56 +0300

    just commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,19 @@ import Image from 'next/image'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 import requests from '../utils/requests'
+import { Movie } from '../typings'
+import Row from '../components/Row'
+
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
 
 export const getServerSideProps = async () => {
   const [
@@ -42,23 +55,35 @@ export const getServerSideProps = async () => {
 
 
 
-const Home = ({netflixOriginals}) => {
-  console.log(netflixOriginals)
+const Home = ({
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  trendingNow,
+  topRated
+  }: Props) => {
+
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
       <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <Header />
-      <main>
-        <Banner />
-        <section>
-          {/* {Row} */}
-          {/* {Row} */}
-          {/* {Row} */}
-          {/* {Row} */}
-          {/* {Row} */}
+      <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
+        <Banner netflixOriginals={netflixOriginals}/>
+        <section className="md:space-y-24">
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
         {/* {modal} */}
       </main>
